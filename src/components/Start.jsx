f54998c107327f6073266b358905b67ca7727704
patrickikhidero <80291797+patrickikhidero@users.noreply.github.com>
2022-02-25 11:41:44 +0100

    quiz app

---
+++ src/components/Start.jsx
@@ -0,0 +1,25 @@
+import { useRef } from "react";
+
+export default function Start({ setUsername }) {
+  const inputRef = useRef();
+
+  const handleClick = () => {
+    inputRef.current.value && setUsername(inputRef.current.value);
+  };
+
+  return (
+    <div className="start">
+    <h1>Do you want to be a Millionaire?</h1><br/>
+      <input
+        className="startInput"
+        placeholder="Then enter your name"
+        ref={inputRef}
+      /><br/>
+      <button className="startButton" onClick={handleClick}>
+        Start
+      </button><br />
+      
+      <p>with <img src="https://img.icons8.com/color/48/000000/like--v3.png"/> by codepreneur</p>
+    </div>
+  );
+}
